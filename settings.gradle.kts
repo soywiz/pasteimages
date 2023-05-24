994c07d972f6af4b6a1953ae71ee86f1d3d8417b
soywiz <soywiz@gmail.com>
2023-05-24 17:14:39 +0200

    Some fixes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,3 @@
-rootProject.name = "PasteImages"
+// For some reason (maybe because changes case of the project name)
+// this causes a problem when loading the project
+//rootProject.name = "PasteImages"
